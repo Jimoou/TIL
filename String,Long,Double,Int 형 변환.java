b61edf0fefcc904d8e018c953f6a011d23bbe58d
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-01 08:06:53 +0900

    Create String,Long,Double,Int 형 변환.java

---
+++ String,Long,Double,Int 형 변환.java
@@ -0,0 +1,13 @@
+//String -> Int
+ String test = "123";
+int num = Integer.valueOf(test); 
+|| 
+String test = "123";
+int num = Integer.parseInt(test);
+
+//Int -> String
+int test = 123;
+String lang = String.valueOf(test);
+||
+int test = 123;
+String lang = String.toString(test);
