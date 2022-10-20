2f37edcdd27fd9607dcde415579c43cac8cf5755
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-20 15:10:46 +0900

    Create 리스트의 평균계산.py

---
+++ python/리스트의 평균계산.py
@@ -0,0 +1,18 @@
+1.
+sum(my_list) / len(my_list)
+
+2.
+import statistics
+
+my_list = [10, 20, 30, 40, 50, 60]
+mean = statistics.mean(my_list)
+print(mean)
+#35
+
+3.
+import numpy
+
+my_list = [10, 20, 30, 40, 50, 60]
+mean = numpy.mean(my_list)
+print(mean)
+#35.0
