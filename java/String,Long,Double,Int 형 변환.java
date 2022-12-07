9e4e25d120f1b84c2554bc18b634a92d5050a24d
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-12-07 16:28:15 +0900

    Update String,Long,Double,Int 형 변환.java

--- java/String,Long,Double,Int 형 변환.java
+++ java/String,Long,Double,Int 형 변환.java
@@ -5,9 +5,34 @@ int num = Integer.valueOf(test);
 String test = "123";
 int num = Integer.parseInt(test);
 
+//String -> Float
+Float.parselFloat();
+
+//String -> Double
+Double.parseDouble();
+
+//String -> Long
+Long.parseLong();
+
 //Int -> String
 int test = 123;
 String lang = String.valueOf(test);
 ||
 int test = 123;
 String lang = String.toString(test);
+
+//Double -> Float
+1
+Double db = new Double(d);
+float f1 = db.floatValue();
+2
+float f = (float) d;
+
+//Double -> String
+1
+double d = 123.45d;
+String str = Double.toString(d);
+2
+double d = 123.456d;
+String str = String.valueOf(d);
+
